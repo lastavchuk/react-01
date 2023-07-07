acbda03b317d6f326add341969d6b73219dc0679
lastavchuk <it.lastavchuk@gmail.com>
2023-07-07 15:05:10 +0300

    add styled

--- src/components/Container/Container.styled.js
+++ src/components/Container/Container.styled.js
@@ -7,6 +7,9 @@ const StyledContainer = styled.div`
     margin-right: auto;
     margin-left: auto;
 
+    padding-left: 10px;
+    padding-right: 10px;
+
     @media screen and (min-width: 768px) {
         max-width: 768px;
         width: 450px;
